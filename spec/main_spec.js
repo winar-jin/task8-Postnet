05820290953aa4ffdfe755b6bd81dfbb2d5c6678
winar-jin <winar1994@outlook.com>
2017-08-21 12:06:11 +0800

    refactor and format the task 8

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -9,36 +9,36 @@ chai.use(sinonChai);
 var main = require("../lib/main.js");
 
 
-describe("将邮编转换为条码：", function(){
+describe("将邮编转换为条码：", function () {
 
-    it('将95713转换为条码：', function(){
-      var result = main.encode('95713');
-      var expect_string = '||:|:::|:|:|:::|:::||::||::|:|:|';
-      expect(expect_string).to.equal(result);
-    });
+  it('将95713转换为条码：', function () {
+    var result = main.encode('95713');
+    var expect_string = '||:|:::|:|:|:::|:::||::||::|:|:|';
+    expect(expect_string).to.equal(result);
+  });
 
-    it('将989808998转换为条码：',function () {
-      var result = main.encode('989808998');
-      var expect_string = '||:|::|::|:|:|::|::|:||:::|::|:|:|::|:|::|::|:::|:||';
-      expect(expect_string).to.equal(result);
-    })
+  it('将989808998转换为条码：', function () {
+    var result = main.encode('989808998');
+    var expect_string = '||:|::|::|:|:|::|::|:||:::|::|:|:|::|:|::|::|:::|:||';
+    expect(expect_string).to.equal(result);
+  })
 
-    it('将98980-8998转换为条码：',function () {
-      var result = main.encode('98980-8998');
-      var expect_string = '||:|::|::|:|:|::|::|:||:::|::|:|:|::|:|::|::|:::|:||';
-      expect(expect_string).to.equal(result);
-    })
+  it('将98980-8998转换为条码：', function () {
+    var result = main.encode('98980-8998');
+    var expect_string = '||:|::|::|:|:|::|::|:||:::|::|:|:|::|:|::|::|:::|:||';
+    expect(expect_string).to.equal(result);
+  })
 });
 
-describe("将条码解码为邮编：", function(){
+describe("将条码解码为邮编：", function () {
 
-  it('将||:|:::|:|:|:::|:::||::||::|:|:|转换为邮编：', function(){
+  it('将||:|:::|:|:|:::|:::||::||::|:|:|转换为邮编：', function () {
     var result = main.decode('||:|:::|:|:|:::|:::||::||::|:|:|');
     var expect_string = '95713';
     expect(expect_string).to.equal(result);
   });
 
-  it('将||:|::|::|:|:|::|::|:||:::|::|:|:|::|:|::|::|:::|:||转换为条码：',function () {
+  it('将||:|::|::|:|:|::|::|:||:::|::|:|:|::|:|::|::|:::|:||转换为条码：', function () {
     var result = main.decode('||:|::|::|:|:|::|::|:||:::|::|:|:|::|:|::|::|:::|:||');
     var expect_string = '98980-8998';
     expect(expect_string).to.equal(result);
